078a494c4f8d93a60fafa13ae2bb13f4c4df0b00
Christoffel Daniel Y. Tambunan <christoffeltambunan54@gmail.com>
2025-03-16 01:53:32 +0700

    Finished the design of the Login Activity

--- app/src/main/java/com/example/antime/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/antime/ui/login/LoginActivity.kt
@@ -1,23 +1,27 @@
 package com.example.antime.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.antime.MainActivity
 import com.example.antime.R
 import com.example.antime.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.2(savedInstanceState)
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        supportActionBar?.hide()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener(){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
